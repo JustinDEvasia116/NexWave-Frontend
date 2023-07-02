d80e0179f9595cb1fc922eb98a062e65eb1e41d1
JustinDEvasia116 <djst.devasia@gmail.com>
2023-07-02 20:03:49 +0530

    error 5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    jsx: true,
+  },
+  server: {
+    mimeTypes: {
+      'text/javascript': ['js'],
+    },
+  },
   resolve: {
       alias: [
       {
